9f08062df36f002e9aaeebf8aa6aec21d4da9d03
sachin1092 <me@sachinshinde.com>
2015-10-25 13:02:18 -0700

    completed move x up

--- main.py
+++ main.py
@@ -3,16 +3,15 @@ import math
 PUZZLE_TYPE = 8
 MAT_SIZE = int(math.sqrt(PUZZLE_TYPE + 1))
 
-mat = [1, 2, 3, 4, 5, 6, 7, 8, -1]
-
 def print_board(mat):
-	print "\n\nBoard:\n"
+	print "\nBoard:"
+	print "*"*15
 	for index, val in enumerate(mat):
 		if (index + 1) % MAT_SIZE == 0:
-			print val
+			print val if val != -1 else "x"
 		else:
-			print val, " ",
-	print "\n"
+			print val if val != -1 else "x", " ",
+	print "*"*15
 
 def can_move_up(mat):
 	return True if mat.index(-1) >= MAT_SIZE else False
@@ -27,10 +26,16 @@ def can_move_right(mat):
 	return False if mat.index(-1) % MAT_SIZE == MAT_SIZE - 1 else True
 
 def move_x_up(mat):
-	pass
+	if can_move_up(mat):
+		print "\nMoving x up"
+		index = mat.index(-1)
+		mat[index - MAT_SIZE], mat[index] = mat[index], mat[index - MAT_SIZE]
+	else:
+		print "\nOperation not allowed"
 
 
 if __name__ == "__main__":
+	mat = [1, 2, 3, 4, 5, 6, 7, 8, -1]
 	# TODO remove this later
 	# print "Enter elements for %d Puzzle." % PUZZLE_TYPE
 	# print "NOTE: Use \"x\" for blank.\n"
@@ -43,3 +48,7 @@ if __name__ == "__main__":
 	print "can move down? ", can_move_down(mat)
 	print "can move left? ", can_move_left(mat)
 	print "can move right? ", can_move_right(mat)
+	move_x_up(mat)
+	print_board(mat)
+	move_x_up(mat)
+	print_board(mat)
