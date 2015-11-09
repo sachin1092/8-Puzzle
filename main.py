787633ce467e5e7b494c0609ddaf7d20babbd036
sachin1092 <me@sachinshinde.com>
2015-11-08 22:38:11 -0800

    completed everything

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import math
-from random import shuffle
 import heapq
+import time
+from operator import itemgetter
 
 PUZZLE_TYPE = 8
 MAT_SIZE = int(math.sqrt(PUZZLE_TYPE + 1))
-
 
 class PriorityQueue(object):
 
@@ -18,12 +18,14 @@ class PriorityQueue(object):
     def empty(self):
         return len(self.elements) == 0
     
-    def put(self, item, h=0, g=0):
-        heapq.heappush(self.elements, (h + g, h, g, item))
+    def put(self, item, h=0, g=0, priority=0):
+        # heapq.heappush(self.elements, (h + g, h, g, item))
+        self.elements.append((priority, h, g, item))
+        self.elements.sort(key=itemgetter(0))
         self.max_elements = self.max_elements if self.max_elements > len(self.elements) else len(self.elements)
 
     def get_item(self):
-        return heapq.heappop(self.elements)
+        return self.elements.pop(0)
 
 class Problem(object):
 
@@ -141,7 +143,7 @@ General search algorithm
 def general_search(problem, queueing_func):
 	depth = 0
 	nodes = PriorityQueue()
-	nodes.put(problem.get_current_state(), 0, 0)
+	nodes.put(problem.get_current_state())
 	while not nodes.empty():
 		entire_node = nodes.get_item()
 		node = entire_node[3]
@@ -150,12 +152,12 @@ def general_search(problem, queueing_func):
 		print_board(node)
 		if problem.goal_test(node): 
 			print "Goal State"
-			print "To solve this problem the search algorithm expanded a total of %d nodes."%problem.get_level()
+			print "To solve this problem the search algorithm expanded a total of %d nodes." % problem.get_level()
 			print "The maximum number of nodes in the queue at any one time was %d."% nodes.get_max_elements()
-			print "The depth of the goal node was %d" % depth
+			print "The depth of the goal node was %d" % entire_node[2]
 			return node
 		print "Expanding this state\n\n"
-		queueing_func(nodes, expand(node, problem))  
+		queueing_func(nodes, expand(entire_node, problem))
 		depth += 1
 	
 """
@@ -163,18 +165,18 @@ Expands the current node using all the operators and returns the queue.
 """
 def expand(node, problem):
 	all_nodes = PriorityQueue()
-	node1 = move_x_up(node[:])
-	node2 = move_x_down(node[:])
-	node3 = move_x_left(node[:])
-	node4 = move_x_right(node[:])
+	node1 = move_x_up(node[3][:])
+	node2 = move_x_down(node[3][:])
+	node3 = move_x_left(node[3][:])
+	node4 = move_x_right(node[3][:])
 	if node1 and not problem.is_explored(node1):
-		all_nodes.put(node1, problem.get_level())
+		all_nodes.put(node1, 0, node[2] + 1, 0)
 	if node2 and not problem.is_explored(node2):
-		all_nodes.put(node2, problem.get_level())
+		all_nodes.put(node2, 0, node[2] + 1, 0)
 	if node3 and not problem.is_explored(node3):
-		all_nodes.put(node3, problem.get_level())
+		all_nodes.put(node3, 0, node[2] + 1, 0)
 	if node4 and not problem.is_explored(node4):
-		all_nodes.put(node4, problem.get_level())
+		all_nodes.put(node4, 0, node[2] + 1, 0)
 	return all_nodes
 
 """
@@ -183,7 +185,7 @@ Queueing function for Unifrom Cost Search.
 def uniform_cost_search(nodes, new_nodes):
 	while not new_nodes.empty():
 		node = new_nodes.get_item()
-		nodes.put(node[3])
+		nodes.put(node[3], 0, node[2], 0)
 
 """
 Calculates the heuristic using the misplaced tile.
@@ -217,7 +219,7 @@ Queueing function for Misplaced Tile Heuristic.
 def misplaced_tile_heuristic(nodes, new_nodes):
 	while not new_nodes.empty():
 		node = new_nodes.get_item()
-		nodes.put(node[3], calculate_misplaced(node[3]), node[0])
+		nodes.put(node[3], calculate_misplaced(node[3]), node[2], calculate_misplaced(node[3]) + node[2])
 
 """
 Queueing function for Manhattan Distance Heuristic.
@@ -225,7 +227,7 @@ Queueing function for Manhattan Distance Heuristic.
 def manhattan_distance_heuristic(nodes, new_nodes):
 	while not new_nodes.empty():
 		node = new_nodes.get_item()
-		nodes.put(node[3], manhattan_distance(node[3]), node[0])
+		nodes.put(node[3], manhattan_distance(node[3]), node[2], manhattan_distance(node[3]) + node[2])
 
 if __name__ == "__main__":
 	print "Welcome to the awesome %d-puzzle solver." % PUZZLE_TYPE
@@ -250,11 +252,17 @@ if __name__ == "__main__":
 	print "*"*50
 	print "Enter your choice of algorithm:\n1. Uniform Cost Search\n2. A* with the Misplaced Tile heuristic.\n3. A* with the Manhattan distance heuristic."
 	choice = int(raw_input())
+	t1 = 0
 	if choice == 1:
+		t1 = time.time()
 		general_search(problem, uniform_cost_search)
 	elif choice == 2:
+		t1 = time.time()
 		general_search(problem, misplaced_tile_heuristic)
 	elif choice == 3:
+		t1 = time.time()
 		general_search(problem, manhattan_distance_heuristic)
 	else:
 		print "Invalid choice."
+	t2 = time.time()
+	print "The algorithm took " + str((t2-t1) * 1000)  + " ms of time."
